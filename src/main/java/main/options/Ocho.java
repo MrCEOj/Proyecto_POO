fec36a05988182a19920fe94fe75eecc0fa2644d
Radames Castro <castro.radames@uabc.edu.mx>
2022-11-23 21:30:24 -0800

    Busqueda numerica

--- src/main/java/main/options/Ocho.java
+++ src/main/java/main/options/Ocho.java
@@ -1,6 +1,9 @@
 package main.options;
+import java.io.*;
 
 public class Ocho extends javax.swing.JPanel {
+    
+    Uno b = new Uno();
 
     public Ocho() {
         initComponents();
@@ -111,7 +114,7 @@ public class Ocho extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
+
     }//GEN-LAST:event_jButton1ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
